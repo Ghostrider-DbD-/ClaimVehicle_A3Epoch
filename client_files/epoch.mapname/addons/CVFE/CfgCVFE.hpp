b7de9357e0542a3479160e7134943848b7f93b54
Chris Cardozo <cardocp@hotmail.com>
2020-10-16 19:53:44 -0400

    Small tweaks to the user settings and added an icon for claiming vehicles

--- client_files/epoch.mapname/addons/CVFE/CfgCVFE.hpp
+++ client_files/epoch.mapname/addons/CVFE/CfgCVFE.hpp
@@ -1,5 +1,19 @@
 
 class CfgCVFE {
 	claimCost = 500;
-	requiredClaimComponents[] = { {"CircuitParts", 1}, {"ItemComboLock",1}};	
+	requiredClaimComponents[] = { 
+		// Easy settings
+		/*
+			// list nothing here
+		*/
+
+		// Medium setting 
+		// List one or two things here 
+		{"CircuitParts", 1}
+
+		// Hard Setting 
+		// Increase claimCost and list several things here 
+		//{"CircuitParts", 1},
+		//{"ItemCorrugated",1}		
+	};	
 };
--- client_files/epoch.mapname/epoch_config/configs/CfgActionMenu/CfgActionMenu_target.hpp
+++ client_files/epoch.mapname/epoch_config/configs/CfgActionMenu/CfgActionMenu_target.hpp
@@ -4,6 +4,6 @@ class CVFE_claimVehicle
 {
 	condition = "dyna_isVehicle && !dyna_inVehicle && !dyna_locked && (local dyna_cursorTarget) && (dyna_cursorTarget getVariable['VEHICLE_SLOT','ABORT'] isEqualTo 'ABORT')";
 	action = "[dyna_cursorTarget] call CVFE_fnc_claimVehicle_client";
-	icon ="\x\addons\a3_epoch_assets_1\pictures\equip_key_CA_yellow.paa";
+	icon =  "\A3\EditorPreviews_F_oldman\Data\CfgVehicles\Land_Key_01_F.jpg";
 	toolTip = "Claim Vehicle";
 };
